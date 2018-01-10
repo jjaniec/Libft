54a9070f6e8331f4a484518f63731b8dcac52336
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-10 15:47:11 +0000

    fix compilation error on linux for ft_str(n)equ -Werror=pointer-to-int-cast

--- srcs/ft_strequ.c
+++ srcs/ft_strequ.c
@@ -18,7 +18,7 @@ int		ft_strequ(char const *s1, char const *s2)
 
 	i = 0;
 	if (!(s1) || !(s2))
-		return ((int)NULL);
+		return (0);
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 		i++;
 	return ((s1[i] == s2[i]) ? (1) : (0));
--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -18,7 +18,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 
 	i = 0;
 	if (!(s1) || !(s2))
-		return ((int)NULL);
+		return (0);
 	if (n == 0)
 		return (1);
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < (n - 1))
