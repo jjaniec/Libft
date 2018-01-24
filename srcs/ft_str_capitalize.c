f431b1084f07433771cf31d7f19f11594144125e
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-24 14:44:22 +0100

    remake ft_str_capitalize.c

--- srcs/ft_str_capitalize.c
+++ srcs/ft_str_capitalize.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 17:42:26 by jjaniec           #+#    #+#             */
-/*   Updated: 2017/12/16 18:05:32 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/24 14:41:46 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 char	*ft_str_capitalize(char *s)
 {
-	char	*t;
+    int     i;
 
-	t = &(*s);
-	while (*s)
-		if (*s >= 'a' && *s <= 'z')
-			*s++ -= 32;
-	return (t);
+    i = -1;
+    while (s[++i])
+        if (s[i] >= 'a' && s[i] <= 'z')
+            s[i] -= 32;
+    return (s);
 }
