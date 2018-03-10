922ab0408fbe5f1c9b982e5d31186973ccfae4e8
jjaniec <jjaniec@student.42.fr>
2018-03-10 19:00:53 +0100

    remove all restrict keywords

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -13,7 +13,7 @@
 #include <strings.h>
 #include "./libft.h"
 
-void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, \
+void	*ft_memccpy(void *dst, const void *src, int c, \
 		size_t n)
 {
 	size_t	i;
