c182ed5ec488cbf0bd0b8efe6a3adaa53a8287a3
Joffrey Janiec <jjaniec@student.42.fr>
2018-07-23 16:03:32 +0200

    remake strsplit

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -3,112 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/10 19:42:58 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/02/01 12:18:50 by jjaniec          ###   ########.fr       */
+/*   Created: 2018/07/23 15:59:56 by jjaniec           #+#    #+#             */
+/*   Updated: 2018/07/23 16:00:04 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft.h"
+#include "libft.h"
 
-static char	**ft_strsplit_ret_tab0wd(void)
+static int		ft_cnt_parts(const char *s, char c)
 {
-	char	**tab0words;
+	int		cnt;
+	int		in_substring;
 
-	if (!(tab0words = (char **)malloc(sizeof(char *) * 2)))
-		return (NULL);
-	if (!(tab0words[0] = (char *)malloc(sizeof(char))))
-		return (NULL);
-	tab0words[0] = NULL;
-	return (tab0words);
-}
-
-static int	ft_strsplit_s_size(char const *s, char c, int i)
-{
-	int		cunt;
-	int		j;
-
-	j = 0;
-	while (i > 0)
+	in_substring = 0;
+	cnt = 0;
+	while (*s != '\0')
 	{
-		cunt = 0;
-		while (s[j] == c && s[j] != '\0')
-			j++;
-		if (s[j] != c && s[j] != '\0')
+		if (in_substring == 1 && *s == c)
+			in_substring = 0;
+		if (in_substring == 0 && *s != c)
 		{
-			while (s[j] != c && s[j] != '\0')
-			{
-				++cunt;
-				++j;
-			}
-			i--;
+			in_substring = 1;
+			cnt++;
 		}
+		s++;
 	}
-	return (cunt);
+	return (cnt);
 }
 
-static int	ft_strsplit_tab_nb(char const *s, char c)
+static int		ft_wlen(const char *s, char c)
 {
-	int		i;
-	int		cunt;
+	int		len;
 
-	cunt = 0;
-	i = 0;
-	while (s[i] != '\0')
+	len = 0;
+	while (*s != c && *s != '\0')
 	{
-		while (s[i] == c && s[i] != '\0')
-			i++;
-		if (s[i] != c && s[i] != '\0')
-		{
-			while (s[i] != c && s[i] != '\0')
-				i++;
-			cunt++;
-		}
+		len++;
+		s++;
 	}
-	return (cunt);
+	return (len);
 }
 
-static char	**ft_strsplit_filltabs(char const *s, char c, char **tab)
+char			**ft_strsplit(char const *s, char c)
 {
-	int		i2;
-	int		j;
-	int		j2;
+	char	**t;
+	int		nb_word;
+	int		index;
 
-	j = 0;
-	i2 = 0;
-	j2 = 0;
-	while (s[i2] != '\0' && j < ft_strsplit_tab_nb(s, c) && j2 > -1)
-	{
-		j2 = 0;
-		while (s[i2] == c)
-			i2++;
-		if (s[i2] != '\0')
-		{
-			tab[j] = ft_strsub(s, i2, ft_strsplit_s_size(s, c, j + 1));
-			while (ft_strcmp(tab[j], ft_strsub(s, i2,\
-							ft_strsplit_s_size(s, c, j + 1))) != 0)
-				tab[j] = ft_strsub(s, i2, ft_strsplit_s_size(s, c, j + 1));
-			i2 += ft_strsplit_s_size(s, c, j + 1);
-			j += 1;
-		}
-		i2 += 1;
-	}
-	tab[j] = 0;
-	return (tab);
-}
-
-char		**ft_strsplit(char const *s, char c)
-{
-	char	**tab;
-	int		tabs;
-
-	if (!(s) || s[0] == '\0')
-		return ((!(s)) ? (NULL) : (ft_strsplit_ret_tab0wd()));
-	tabs = ft_strsplit_tab_nb(s, c);
-	if (!(tab = (char **)malloc(sizeof(char) * tabs + 1)))
+	index = 0;
+	nb_word = ft_cnt_parts((const char *)s, c);
+	t = (char **)malloc(sizeof(*t) * (ft_cnt_parts((const char *)s, c) + 1));
+	if (t == NULL)
 		return (NULL);
-	tab = ft_strsplit_filltabs(s, c, tab);
-	tab = ft_strsplit_filltabs(s, c, tab);
-	return (tab);
+	while (nb_word--)
+	{
+		while (*s == c && *s != '\0')
+			s++;
+		t[index] = ft_strsub((const char *)s, 0, ft_wlen((const char *)s, c));
+		if (t[index] == NULL)
+			return (NULL);
+		s = s + ft_wlen(s, c);
+		index++;
+	}
+	t[index] = NULL;
+	return (t);
 }
